640a3ede8897b20a39d9cd0468d4f80a50ef7a2d
Milena Hikari <milenahikari11@gmail.com>
2018-12-07 21:10:22 -0200

    Criando cabecalho e rodape

---
+++ cabecalho.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>Blog Maravilha</title>
+        <meta charset ="UTF-8">
+        <link rel="shortcut icon" href="assets/img/logo-coala.jpg"  type="image/ico" />
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/notas.css">
+
+       <!-- JAVASCRIPT E JQUERY -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background:linear-gradient(45deg, #34ebe9, #d0ffae);">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">EVERNotas</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">       
+                        <li class="nav-item active">
+                            <a class="nav-link" href="nota-lista.php">Notas<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="nota-criar.php">Criar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="nota-editar.php">Editar</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="principal">
--- nota-editar.php
+++ nota-editar.php
@@ -1,3 +1,3 @@
 <?php include("cabecalho.php");?>
-    <h1>Bem Vindo!</h1>
+    <h1>Nota Editar!</h1>
 <?php include("rodape.php");?>        
