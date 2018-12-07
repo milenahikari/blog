4c68052a63f53cb7458b3d0451589041ccd2ead1
Milena Hikari <milenahikari11@gmail.com>
2018-12-07 21:36:00 -0200

    Criando criar notas

--- nota-criar.php
+++ nota-criar.php
@@ -1,3 +1,26 @@
 <?php include("cabecalho.php");?>
-    <h1>Nota criar!</h1>
+    <h1>Nova Nota de Texto</h1>
+    <form>
+        <table class="table">
+            <tr>
+                <td>Título:</td>
+                <td><input class="form-control" type="text" name="titulo"></td>
+            </tr>
+            <tr>
+                <td>Autor:</td>
+                <td><input class="form-control" type="text" name="autor"></td>
+            </tr>
+            <tr>
+                <td>Data:</td>
+                <td><input class="form-control" type="date" name="data"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input class="form-control" type="text" name="corpo-nota"></td>
+            </tr>
+            <tr>
+                <td><button class="btn btn-primary" type="submit">Criar Nota</button></td>
+            </tr>
+        </table>
+    </form>
 <?php include("rodape.php");?>        
